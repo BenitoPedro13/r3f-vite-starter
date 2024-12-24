4f7266dab001a699d040425bb322126a7d02ab77
Benito Pedro <benitopedro21@outlook.com>
2024-12-24 13:24:06 -0300

    📦 NEW: Exploding Objects

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,44 @@
-import { OrbitControls } from "@react-three/drei";
-
+import {
+  Billboard,
+  Environment,
+  Float,
+  OrbitControls,
+  useTexture,
+} from "@react-three/drei";
+import { Model } from "./Fuckoff.jsx";
+import { BananaModel } from "./Banana.jsx";
+import { WesternBluebird } from "./BlueBird";
+import { useControls } from "leva";
 export const Experience = () => {
+  const { item } = useControls({
+    item: {
+      value: "triangularSphere",
+      options: ["triangularSphere", "banana", "bird"],
+    },
+  });
+
+  const xLogo = useTexture("/textures/x-logo.png");
+
   return (
     <>
-      <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <OrbitControls enableZoom={false} />
+      <Float floatIntensity={2} speed={3}>
+        <Model scale={0.01} visible={item === "triangularSphere"} />
+        <BananaModel scale={0.01} visible={item === "banana"} />
+        <WesternBluebird
+          scale={1.34}
+          rotation-y={-Math.PI / 4}
+          visible={item === "bird"}
+        />
+
+        <Billboard visible={item === "bird"}>
+          <mesh>
+            <planeGeometry args={[0.8, 0.8]} />
+            <meshBasicMaterial map={xLogo} transparent />
+          </mesh>
+        </Billboard>
+      </Float>
+      <Environment preset="sunset" background blur={0.4} />
     </>
   );
 };
